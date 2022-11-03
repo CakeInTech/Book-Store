01100cba76610826a6c424a4f27b36ed820f9de5
MohamedCK <abdulhakimm280@gmail.com>
2022-11-03 13:25:41 +0200

    Updatethe book component

--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -1,34 +1,20 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styles from './Books.module.css';
 import Addbook from '../Addbook/Addbook';
+import Book from './Book';
 
 function Books() {
-  const [Books, setBooks] = useState({
+  const [books, setBooks] = useState([{
     id: 1,
     title: 'The book of dungeons',
     author: 'Harry Potter',
-  });
-
-  const getBooks = () => {
-    setBooks({ id: 1, title: 'The book of dungeons', author: 'Harry Pottter' });
-  };
-  console.log(getBooks);
-
-  const { id, title, author } = Books;
+  }]);
 
   return (
     <div className={styles.container}>
       <div className={styles.bookList}>
         <ul className={styles.item}>
-          <li>
-            {id}
-            ,
-            {' '}
-            {title}
-            ,
-            {' '}
-            {author}
-          </li>
+          {books && books.map((book) => (<Book key={book.id} book={book} />))}
         </ul>
       </div>
       <Addbook />
--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function Categories() {
-  <div>
-    <button type="button">Check Status</button>
-  </div>;
+  return (
+    <div>
+      <button type="button">Check Status</button>
+    </div>
+  );
 }
 
 export default Categories;
