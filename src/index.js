0040118a16bc4a65f09e732271bd709269ab48b1
MohamedCK <abdulhakimm280@gmail.com>
2022-11-08 16:39:32 +0200

    Import the `Provider` and initialized it's component to hold the app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './Components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </Provider>,
 );
