d064ab3ba3efaae870da5adfae63152a4dac6b9a
MohamedCK <abdulhakimm280@gmail.com>
2022-11-08 17:14:22 +0200

    initialize the random id generator

--- src/Components/Addbook/Addbook.jsx
+++ src/Components/Addbook/Addbook.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import styles from './Addbook.module.css';
 
 function Addbook() {
-  const authorRef = useRef();
-  const titleRef = useRef();
+  const authorRef = useRef(bookObj);
+  const titleRef = useRef(bookObj);
   const dispatch = useDispatch();
 
   const [bookObj, setBookObj] = useState({
@@ -17,10 +18,12 @@ function Addbook() {
     added.preventDefault();
     const title = titleRef.current.value;
     const author = authorRef.current.value;
+    const id = uuidv4();
     setBookObj((previusState) => ({
       ...previusState,
       title,
       author,
+      id,
     }));
   };
 
