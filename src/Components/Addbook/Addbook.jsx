72bc65ad101c45e84c299d52a410b5a6cd96fc0d
MohamedCK <abdulhakimm280@gmail.com>
2022-11-08 16:58:28 +0200

    used the refs hook and added the dispatch hook

--- src/Components/Addbook/Addbook.jsx
+++ src/Components/Addbook/Addbook.jsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { useDispatch } from 'react-redux';
 import styles from './Addbook.module.css';
 
 function Addbook() {
+  const authorRef = useRef();
+  const titleRef = useRef();
+  const dispatch = useDispatch();
+
+  const [bookObj, setBookObj] = useState({
+    id: null,
+    title: null,
+    author: null,
+  });
+
+  const addBookHandler = (added) => {
+    console.log(added);
+  };
+
   return (
     <div className={styles.container}>
       <p className={styles.miniheader}>Add New Book</p>
-      <form className={styles.formContainer}>
-        <input type="text" placeholder="Title of the book" className={styles.input} />
-        <input type="text" placeholder="author name" className={styles.input} />
-        <input type="text" placeholder="category" className={styles.input} />
-        <button type="button" value="Add" label="Add" className={styles.addBtn}>Add Book</button>
+      <form
+        className={styles.formContainer}
+        onSubmit={addBookHandler}
+      >
+        <input
+          type="text"
+          placeholder="Title of the book"
+          className={styles.input}
+          ref={titleRef}
+        />
+        <input
+          type="text"
+          placeholder="author name"
+          className={styles.input}
+          ref={authorRef}
+        />
+        <input
+          type="text"
+          placeholder="category"
+          className={styles.input}
+        />
+        <button
+          type="button"
+          value="Add"
+          label="Add"
+          className={styles.addBtn}
+        >
+          Add Book
+        </button>
       </form>
     </div>
   );
