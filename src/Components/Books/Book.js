5d3990dea19a1741daea9fe8c8c2ead8a1d851fd
MohamedCK <abdulhakimm280@gmail.com>
2022-11-08 18:29:56 +0200

    Dispatch the remove action

--- src/Components/Books/Book.js
+++ src/Components/Books/Book.js
@@ -2,18 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // eslint-disable-next-line react/prop-types
-function Book({ title, author }) {
+function Book({ title, author, removeBookHandler }) {
   return (
-    <li>
-      <h2>{title}</h2>
-      <p>{author}</p>
-    </li>
+    <>
+      <li>
+        <h2>{title}</h2>
+        <p>{author}</p>
+      </li>
+      <button
+        type="button"
+        onClick={() => {
+          removeBookHandler();
+        }}
+      >
+        Delete
+      </button>
+    </>
   );
 }
 
-Book.defaultProps = {
-  title: PropTypes.string,
-  author: PropTypes.string,
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
 };
 
 export default Book;
--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -1,17 +1,29 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+// Action Creator
+import { removeBook } from '../../redux/books/books';
 import styles from './Books.module.css';
 import Addbook from '../Addbook/Addbook';
 import Book from './Book';
 
 function Books() {
   // eslint-disable-next-line no-unused-vars
+  const dispatch = useDispatch();
   const books = useSelector((state) => state.bookReducer);
+  const removeBookHandler = (object) => {
+    dispatch(removeBook(object));
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.bookList}>
         <ul className={styles.item}>
-          {books && books.map((book) => (<Book key={book.id} book={book} />))}
+          {books && books.map((book) => (
+            <Book
+              key={book.id}
+              book={book}
+              onClick={removeBookHandler(book)}
+            />
+          ))}
         </ul>
       </div>
       <Addbook />
